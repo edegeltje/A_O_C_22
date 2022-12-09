1822585e6d4a56e63af4184065722708ce50affc
Blizzard_inc <thrashy4inbox@gmail.com>
2022-12-09 12:41:19 +0100

    got the first part right yay

---
+++ day2/app/Main.hs
@@ -0,0 +1,34 @@
+module Main where
+
+main :: IO ()
+main = interact (show . sum . map (matchPoints . guideToMatches) . lines)
+
+data RPS = Rock | Paper | Scissors
+  deriving (Eq, Show)
+
+toRPS :: Char -> RPS
+toRPS 'A' = Rock
+toRPS 'B' = Paper
+toRPS 'C' = Scissors
+toRPS 'X' = Rock
+toRPS 'Y' = Paper
+toRPS 'Z' = Scissors
+toRPS _ = undefined
+
+type Match = (RPS,RPS)
+
+points :: RPS -> Integer
+points Rock = 1
+points Paper = 2
+points Scissors = 3
+
+matchPoints :: Match -> Integer
+matchPoints (a,b) = calculate (points b, points a)
+
+calculate :: (Integer, Integer) -> Integer
+calculate (a,b) = a + 3 * ((1 + a - b) `mod` 3)
+
+guideToMatches :: String -> Match
+guideToMatches [a,' ',b] = (toRPS a,toRPS b) 
+guideToMatches _ = undefined
+
